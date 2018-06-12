3d09d28a4de5d591e6513cfe245599ebfc3f7b0d
jdjuan <david.juanherrera@gmail.com>
2018-06-12 17:14:57 -0500

    add person feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Person } from './person.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app';
+  juan: Person;
+
+  constructor() {
+    this.juan = new Person('0', 'Juan Herrera', 25);
+  }
 }
